518f87b81acf3a2eae438f847e8097e986798240
Bityacht-Developer <55692615+dennislee928@users.noreply.github.com>
2024-06-05 19:44:59 +0800

    Create print_statement_explanation.py

---
+++ print_statement_explanation.py
@@ -0,0 +1,4 @@
+# print statements
+variable : "right in the strings!" # type: ignore
+print(f"f strings allow you to embed variables {variable}")
+#f strings allow you to embed variables right in the strings!
